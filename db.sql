f5995098ca9fc09d9f0194e57294812ac3002a61
CHIOMA SARAH OSUJI <78868674+ChiomaSarah@users.noreply.github.com>
2022-02-05 18:01:57 +0100

    heroku

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE jekalo;
+-- CREATE DATABASE jekalo;
 
 CREATE TABLE  credentials(
 user_id SERIAL PRIMARY KEY,
@@ -8,4 +8,4 @@ username VARCHAR(75) UNIQUE NOT NULL,
 date_of_birth VARCHAR(200) NOT NULL
 );
 
-INSERT INTO credentials(first_name, last_name, username, date_of_birth) VALUES('Sarah', 'Osuji', 'Exquisite', '25-10-2022');
+-- INSERT INTO credentials(first_name, last_name, username, date_of_birth) VALUES('Sarah', 'Osuji', 'Exquisite', '25-10-2022');
